1eefdd80ccf30bf435fb89bab7439b3dd5d9d569
sabilafirdias <sabilafirdias@gmail.com>
2025-11-04 14:34:13 +0700

    membuat app.post

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const db = require('./models');
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+db.sequelize.sync().then((result) => {
+    app.listen(3000, () => {
+        console.log('Server Started');
+    })
+})
+    .catch((err) => {
+        console.log(err);
+})
+
+app.post('/kandang', async (req, res) => {
+    const data = req.body;
+    try{
+        const kandang = await db.Kandang.create(data);
+        res.send(kandang);
+    } catch (error) {
+        res.status(500).send({message: error.message});
+    }
+});
+
